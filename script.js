2b0ee8975cc923b5fd6b89e3e322ba9e85c07833
Mansi-sketch <129894321+Mansi-sketch@users.noreply.github.com>
2025-06-22 01:07:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 function mincost(arr)
 { 
-//write your code here
-// return the min cost
-  
+	arr.sort((a,b)=> a-b);
+	let sum1 = 0;
+	while (arr.length) {
+		let first = arr.shift();
+		let second = arr.shift();
+
+		let cost = first + second;
+		sum1 += cost;
+
+		arr.push(cost);
+		arr.sort((a,b)=>a-b);
+		
+	}
+	return sum1;
 }
 
 module.exports=mincost;
